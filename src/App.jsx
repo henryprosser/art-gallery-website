8ebccf404b57376c2bec6194628addf0ede0ec9b
henryprosser <henryprosser@yahoo.co.uk>
2025-07-04 14:51:11 -0700

    Add routing and pages for Home and Location; update dependencies

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,18 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home.jsx";
+import Location from "./pages/Location.jsx";
+
 function App() {
-  return <></>;
+  return (
+    <BrowserRouter>
+      {/* Header */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/location" element={<Location />} />
+      </Routes>
+      {/* Nav */}
+    </BrowserRouter>
+  );
 }
 
 export default App;
