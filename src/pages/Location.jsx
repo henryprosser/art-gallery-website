91e9bbdf1cf6ff1d3466c6ab01ebaa4ce4297c82
henryprosser <henryprosser@yahoo.co.uk>
2025-07-08 17:52:25 -0700

    Create Footer section and add to Location page

--- src/pages/Location.jsx
+++ src/pages/Location.jsx
@@ -1,11 +1,15 @@
 import Map from "../components/Location/Map";
 import Info from "../components/Location/Info";
+import FooterLocation from "../components/Location/FooterLocation";
 
 export default function Location() {
   return (
-    <main>
-      <Map />
-      <Info />
-    </main>
+    <div>
+      <main>
+        <Map />
+        <Info />
+      </main>
+      <FooterLocation />
+    </div>
   );
 }
