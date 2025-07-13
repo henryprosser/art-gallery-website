6849ef7907cae5f7a90c5fe2ed0f163d23d80cd7
henryprosser <henryprosser@yahoo.co.uk>
2025-07-13 14:08:43 -0700

    Integrate motion into hero section

--- src/components/Home/Hero.jsx
+++ src/components/Home/Hero.jsx
@@ -1,14 +1,24 @@
 import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
 
 export default function Hero() {
   return (
-    <section className="md:flex md:relative">
-      <div className="hidden lg:block lg:bg-black w-[450px]">
-        <h1 className="hidden text-preset-1 lg:block lg:text-white lg:absolute md:top-1/3 md:left-[165px] md:translate-y-[-50%] lg:mix-blend-difference">
+    <section className="md:flex md:relative lg:bg-[linear-gradient(90deg,_#151515_0%_50%,_#ffffff_50%_100%)]">
+      <div className="hidden lg:block lg:bg-grey-900 w-[450px]">
+        <motion.h1
+          initial={{ y: -150, opacity: 0 }}
+          animate={{ y: 0, opacity: 1 }}
+          transition={{ duration: 2, ease: "easeOut" }}
+          className="hidden text-preset-1 md:top-1/3 md:left-[165px] md:translate-y-[-50%] lg:block lg:text-white lg:absolute lg:mix-blend-difference lg:z-10"
+        >
           MODERN <br /> ART GALLERY
-        </h1>
+        </motion.h1>
       </div>
-      <picture>
+      <motion.picture
+        initial={{ y: "100%" }}
+        animate={{ y: "0%" }}
+        transition={{ duration: 2, ease: "easeOut" }}
+      >
         <source
           media="(min-width: 1440px)" // lg breakpoint
           srcSet="src/assets/desktop/image-hero@2x.jpg"
@@ -22,9 +32,14 @@ export default function Hero() {
           alt="Art Gallery Exhibition"
           className="w-full h-auto object-cover md:w-[437px] lg:w-[540px]"
         />
-      </picture>
+      </motion.picture>
       {/* Gallery Info */}
-      <div className="flex flex-col gap-[var(--spacing-32)] mt-[var(--spacing-32)] mr-[var(--spacing-16)] ml-[var(--spacing-16)] md:absolute md:top-1/2 md:left-[389px] md:translate-y-[-50%] md:mx-0 md:my-0 md:mr-[var(--spacing-40)] md:gap-[var(--spacing-48)] lg:gap-[var(--spacing-64)] lg:left-[924px] lg:w-[350px] lg:translate-y-[-55%] lg:mr-0">
+      <motion.div
+        initial={{ y: -150, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        transition={{ duration: 2, ease: "easeOut" }}
+        className="flex flex-col gap-[var(--spacing-32)] mt-[var(--spacing-32)] mr-[var(--spacing-16)] ml-[var(--spacing-16)] md:absolute md:top-1/2 md:left-[389px] md:translate-y-[-50%] md:mx-0 md:my-0 md:mr-[var(--spacing-40)] md:gap-[var(--spacing-48)] lg:gap-[var(--spacing-64)] lg:left-[924px] lg:w-[350px] lg:translate-y-[-55%] lg:mr-0"
+      >
         <h1 className="text-preset-1 lg:hidden">MODERN ART GALLERY</h1>
         <p className="text-preset-7 text-grey-700 text-desktop-lg">
           The arts in the collection of the Modern Art Gallery all started from
@@ -46,7 +61,7 @@ export default function Hero() {
             ></img>
           </span>
         </Link>
-      </div>
+      </motion.div>
     </section>
   );
 }
